2430f42ec607de17cda25653e5c2dc2db50d87c0
Savya <88125534+Victrux3021@users.noreply.github.com>
2026-06-28 13:44:07 +0530

    Delete ProjA directory

--- ProjA/backend/routes/analysisRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../middleware/upload");
-const { analyzeExpenses } = require("../controllers/analysisController");
-
-// POST /api/analyze
-router.post("/analyze", upload.single("file"), analyzeExpenses);
-
-module.exports = router;
